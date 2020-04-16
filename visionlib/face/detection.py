cb67c140964f249334564aa61fbb1b638edad793
Ashwin Vinod <ashwinvinodsa@gmail.com>
2020-04-16 23:51:51 +0530

    Modified detection.py to support video files

--- visionlib/face/detection.py
+++ visionlib/face/detection.py
@@ -1,4 +1,6 @@
 import cv2
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 from ..utils.imgutils import Image
 from .haar_detector import HaarDetector
 from .hog_detector import Hog_detector
@@ -22,27 +24,44 @@ class Detector:
         elif detector == "mtcnn":
             self.detector = self.mtcnn
 
-    def detect_face(self, img_path=None, webcam=False, video_path=None):
+    def detect_face(self, img_path=None, webcam=False, video_path=None, show=False):
+
         if webcam is not False:
             web_vid = cv2.VideoCapture(0)
+            box_lst = []
             while True:
                 status, frame = web_vid.read()
                 box = self.detector.detect(img=frame)
-                frame = cv2.rectangle(
-                    frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2
-                )
-                cv2.imshow("Smart Eye", frame)
-                cv2.waitKey(0)
+                box_lst.append(box)
+                for face in box:
+                    frame = cv2.rectangle(
+                        frame, (face[0], face[1]), (face[2], face[3]), (0, 255, 0), 2
+                    )
+
+                if show is True:
+                    cv2.imshow("Smart Eye", frame)
+                    cv2.waitKey(0)
+                else:
+                    yield box_lst
+
         elif video_path is not None:
             video = self.image_util.read_video(video_path)
+            box_lst = []
             while True:
                 status, frame = video.read()
                 box = self.detector.detect(img=frame)
-                frame = cv2.rectangle(
-                    frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2
-                )
-                cv2.imshow("Smart Eye", frame)
-                cv2.waitKey(0)
+                box_lst.append(box)
+                for face in box:
+                    frame = cv2.rectangle(
+                        frame, (face[0], face[1]), (face[2], face[3]), (0, 255, 0), 2
+                    )
+
+                if show is True:
+                    cv2.imshow("Smart Eye", frame)
+                    cv2.waitKey(0)
+                else:
+                    yield box_lst
+
         elif img_path is not None:
             frame = self.image_util.read_img(img_path)
             box = self.detector.detect(img=frame)
@@ -51,5 +70,6 @@ class Detector:
                     frame, (face[0], face[1]), (face[2], face[3]), (0, 255, 0), 2
                 )
             return box
+
         else:
             raise Exception("No Arguments given")
