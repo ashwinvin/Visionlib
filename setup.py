135c6df7acde289bb4fae479c903beb0e72b7033
ashwinvin <38067089+ashwinvin@users.noreply.github.com>
2020-04-26 01:05:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['mtcnn', 'opencv-python', ' dlib', 'numpy'],
+    install_requires=['mtcnn', 'opencv-python', ' dlib', 'wget', 'numpy'],
     python_requires=">=3.6",
 )
