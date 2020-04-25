0816318485076a75b78cb9d79a34e07685f732c1
Ashwin Vinod <ashwinvinodsa@gmail.com>
2020-04-26 00:43:53 +0530

    Setup fully configured

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ with open("README.md", "r") as rd:
 
 setuptools.setup(
     name="visionlib",
-    version="0.6",
+    version="1.0.0",
     author="Ashwin Vinod",
     author_email="ashwinvinodsa@gmail.com",
-    description="This library will make your life much easier",
+    description="A simple, easy to use and customizeble cv library ",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['mtcnn', 'opencv-python', ' dlib', 'numpy'],
     python_requires=">=3.6",
 )
