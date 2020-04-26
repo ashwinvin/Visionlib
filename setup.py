c60df378559b044ae57407f90c3d336149eb3897
ashwinvin <38067089+ashwinvin@users.noreply.github.com>
2020-04-27 01:08:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author="Ashwin Vinod",
     author_email="ashwinvinodsa@gmail.com",
     url='https://github.com/ashwinvin/Visionlib',
-    download_url='https://github.com/ashwinvin/Visionlib/archive/v1.0.0.tar.gz',
+    download_url='https://github.com/ashwinvin/Visionlib/archive/v1.2.0.tar.gz',
     description="A simple, easy to use and customizeble cv library ",
     long_description=long_description,
     long_description_content_type="text/markdown",
