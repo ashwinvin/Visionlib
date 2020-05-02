450b2471c2f06f7973abaefb35f79102156f8b5f
Ashwin Vinod <ashwinvinodsa@gmail.com>
2020-05-02 10:37:11 +0530

    new dependecies and getting ready for new release

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as rd:
 
 setuptools.setup(
     name="visionlib",
-    version="1.3.0",
+    version="1.4.5",
     author="Ashwin Vinod",
     author_email="ashwinvinodsa@gmail.com",
-    url='https://github.com/ashwinvin/Visionlib',
-    download_url='https://github.com/ashwinvin/Visionlib/archive/v1.3.0.tar.gz',
+    url="https://github.com/ashwinvin/Visionlib",
+    download_url="https://github.com/ashwinvin/Visionlib/archive/v1.4.5.tar.gz",
     description="A simple, easy to use and customizeble cv library ",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    keywords=['Deep learning', 'Vision', 'cv'],
+    keywords=["Deep learning", "Vision", "cv"],
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
@@ -21,6 +21,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['mtcnn', 'opencv-python', ' dlib', 'wget', 'numpy'],
+    install_requires=[
+        "mtcnn", "opencv-python", " dlib", "wget", 'numpy', "pafy",
+        'youtube-dl'
+    ],
     python_requires=">=3.6",
 )
